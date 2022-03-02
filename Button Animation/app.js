4818ee3b3838537a6e6e7eeff39e80778936b9a2
satisshTechie <satisshTechie@gmail.com>
2022-03-02 15:19:48 +0530

    Initial Commit

---
+++ Button Animation/app.js
@@ -0,0 +1,8 @@
+const sign_up = document.querySelector('.signup_btn');
+const Icon = document.querySelector('.fa-solid');
+
+sign_up.addEventListener('click',signupButton);
+
+function signupButton(){
+    this.classList.add('Icon');
+}
---
+++ Modal/app.js
@@ -0,0 +1,21 @@
+function onDelete() {
+    let confirmation = document.getElementById("confirmation");
+    if (!confirmation.classList.contains("modal-open")) {
+      confirmation.classList.add("modal-open");
+    }
+  }
+
+  function onCancel() {
+    let confirmation = document.getElementById("confirmation");
+    confirmation.classList.remove("modal-open");
+  }
+
+ 
+  document.addEventListener("DOMContentLoaded", () => {
+    document
+      .getElementById("confirmation")
+      .addEventListener("click", onCancel);
+    document
+      .querySelector(".modal")
+      .addEventListener("click", (e) => e.stopPropagation());
+  });
